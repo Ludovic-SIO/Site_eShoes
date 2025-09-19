0767c46968cbd3a7ac9853667bce6552e0424d26
Champrobert Ludovic <ludovic.champrobert@edouardgand.fr>
2025-09-19 14:14:00 +0200

    troisième Commit

--- migrations/Version20250919064344.php
+++ migrations/Version20250919064344.php
@@ -21,7 +21,6 @@ final class Version20250919064344 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_64C19C15E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
